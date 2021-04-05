b03160d34bca1060f9953d9309ecafbd8d29e895
jithin-kg <jithinjkg@gmail.com>
2021-04-05 22:34:17 +0530

    added block settings activity

---
+++ app/src/main/java/com/nibble/hashcaller/view/ui/manageblock/BlockManageActivity.kt
@@ -0,0 +1,12 @@
+package com.nibble.hashcaller.view.ui.manageblock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nibble.hashcaller.R
+
+class BlockManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_block_manage)
+    }
+}
--- app/src/main/java/com/nibble/hashcaller/view/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/nibble/hashcaller/view/ui/settings/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.nibble.hashcaller.view.ui.settings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.nibble.hashcaller.R
+import com.nibble.hashcaller.view.ui.manageblock.BlockManageActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 
 
@@ -18,13 +20,23 @@ class SettingsActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun initListeners() {
         imgBtnBackMain.setOnClickListener(this)
+        layoutManageBlocking.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when(v?.id){
+            R.id.layoutManageBlocking ->{
+                startBlockManageActivity()
+            }
             R.id.imgBtnBackMain ->{
                 finish()
             }
         }
+    }
+
+    private fun startBlockManageActivity() {
+        val intent = Intent(this, BlockManageActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
     }
 }
